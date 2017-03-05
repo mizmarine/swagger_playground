bcb8e5dc04a6fc2e82291014487771561ccc56ec
mizmarine <masa.guruto+marine@gmail.com>
2017-03-05 22:00:56 +0900

    refactor: test detail

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -11,11 +11,20 @@ class TestRequestBodyValidator(object):
         schema = spec['paths']['/member']['post']['parameters'][0]['schema']
         self.validator = Draft4Validator(schema, format_checker=draft4_format_checker)
 
-    def test_name(self):
-        data = {'name': 'masa'}
+    def assert_success(self, data):
         assert self.validator.validate(data) is None
 
-    def test_without_name(self):
-        data = {}
+    def assert_fail(self, data):
         with pytest.raises(ValidationError):
             self.validator.validate(data)
+
+    def test_name(self):
+        self.assert_success({'name': 'masa'})
+        self.assert_success({'name': '123 '})
+        self.assert_success({'name': ' '})
+
+    def test_empty_name(self):
+        self.assert_fail({'name': ''})
+
+    def test_without_name(self):
+        self.assert_fail({})
